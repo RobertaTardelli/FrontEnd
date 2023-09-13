834d88199ed534de518855763310a76a8fa90bb0
Roberta <142239752+RobertaTardelli@users.noreply.github.com>
2023-09-12 21:52:33 -0300

    lista

---
+++ jogo_da_adivinhacao-main/arquivos/referencias/logica-js-projeto_inicial/app2.js
@@ -0,0 +1,115 @@
+function exercicio1() {
+
+   let numero = prompt("Digite um numero:");
+
+   if (numero == 0) {
+      alert("É ZERO ");
+   }
+   if (numero % 2 == 1) {
+      alert("O número é impar");
+   }
+   if (numero % 2 == 0) {
+      alert("O número é par");
+   }
+}
+
+function exercicio2() {
+
+   numero1 = parseInt(prompt("Primeiro número"));
+   numero2 = parseInt(prompt("Segundo número"));
+
+   if (numero1 > numero2) {
+      alert(`${numero1} é maior que ${numero2}`);
+   }
+   else if (numero1 < numero2) {
+      alert(`${numero1} é menor que ${numero2}`);
+   }
+   else if (numero1 == numero2) {
+      alert(`${numero1} é igual a ${numero2}`);
+   }
+
+}
+
+function exercicio3() {
+
+   let idoso = prompt("Voce é maior de 65 anos? (sim ou não)");
+   let deficiente = prompt("Voce é deficiente físico? (sim ou não)");
+   let gestante = prompt("Voce é gestante? (sim ou não)");
+
+   if (idoso == "sim" || deficiente == "sim" || gestante == "sim") {
+      alert(" Fila Preferencial");
+
+   } else {
+      alert("Fila Comum");
+   }
+
+}
+
+function exercicio4() {
+
+   let idade = parseInt(prompt("Qual é a sua idade?"));
+
+   if (idade < 16) {
+      alert("Entrada Proibida");
+   }
+   else if (idade == 16 || idade == 17 || idade == 18) {
+      alert("Entrada Liberada com Acompanhante");
+   }
+   else {
+      alert("Entrada Liberada");
+   }
+
+}
+function exercicio5() {
+
+   let login = ("Henrique");
+   let senha = ("1234");
+   let loginDigitado = prompt("Digite seu nome!");
+   let senhaDigitada = prompt("Digite sua senha!");
+   let acerto = 0;
+   let erro = 0;
+
+   if ((loginDigitado == login) && (senhaDigitada == senha)) {
+      alert("Bem Vindo!");
+ }
+ else if ((loginDigitado != login) && (senhaDigitada != senha)){
+   alert("Login e Senha Incorretos");
+ }
+    else if (loginDigitado != login){
+      alert("Login incorreto!");
+ }
+    else {
+      alert("Senha Incorreta!");
+
+    }
+   }
+
+   function exercicio6(){
+
+      let primeiraPergunta = prompt("Qual a cor do cavalo Branco de Napoleão? \n1. cavalo \n2. branco \n3. Napoleão");
+      let segundaPergunta = prompt("O Rato do Rei de Roma roeu o que? \n1. roupa \n2. preto \n3. pé");
+      let terceiraPergunta = promt("QUEM DESCOBRIU O BRASIL? \n1. Indios \n2. Cabral \n3. Colombo" );
+
+      if(primeiraPergunta == "2"){
+         acerto++;
+       }else {
+            erro++;
+
+       }if (segundaPergunta == "1") {
+         acerto++;
+      }else{
+         erro++;
+      }if (terceiraPergunta == "2"){
+         acerto++;
+      }else{
+         erro++;
+      alert(acerto);
+   }
+
+   
+
+   }
+exercicio6()
+
+
+
