1c761c287e45471bc0b2e87c16ffc0b18a2c316a
Roberta <142239752+RobertaTardelli@users.noreply.github.com>
2023-09-14 21:49:50 -0300

    lista 3

---
+++ FrontEnd/arquivos/referencias/logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert ("Olá Mundo")
+
+function sayHello(){
+console.log ("Hello, " + Henrique + "!");
+
+var numeroSecreto = 4;
+var chute;
+
+chute = prompt("Chute um número de 1 a 10 ");
+//compara o chute com o numero
+if (chute == numeroSecretoDaDuda){
+   alert("Voce acertou!");
+
+}else{
+    alert("Voce errou!");   
+    if (chute > numeroSecretoDaDuda){
+        alert("O número secreto é menor ")
+  
+} else{
+ alert ("O numero secreto é maior") 
+}
+}
+}
